fb146b82ca3551cfb5d07fd496da3fa66706dbf7
adeoluseyi1 <adeoluseyi1@gmail.com>
2023-07-13 13:34:31 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*Return; 0 (success)
+*main - this is the main function
+*
+*Return:always 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
