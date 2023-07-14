6473e19b7533d6f946666bc22f9714d046510dac
adeoluseyi1 <adeoluseyi1@gmail.com>
2023-07-14 16:42:58 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - this is the main function
+*
+* Return:always 0
+*/
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+
+	while
+
+	    (c <= 'z') {
+	putchar(c);
+	c++;
+	}
+
+	while
+		(d <= 'A") {
+
+		putchar(d);
+		d++;
+		}
+
+	putchar('\n');
+
+	return (0);
+}
