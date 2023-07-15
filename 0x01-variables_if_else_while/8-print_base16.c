49252e0df18f9e1a11b6ad141b8508c109c5f6bd
adeoluseyi1 <adeoluseyi1@gmail.com>
2023-07-14 19:22:34 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - this is the main function
+*
+* Return:always 0
+*/
+
+int main(void)
+{
+
+	int i;
+	{
+	for (i = 48; i < 58; i++)
+	putchar(i);
+	}
+
+	{
+		for (i = 97; i < 103; i++)
+	putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
