aa797f1373d4ce0d5cc1faf980cc9dc6330550ad
adeoluseyi1 <adeoluseyi1@gmail.com>
2023-08-24 13:24:20 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,14 +9,12 @@
 */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-	count++;
-	h = h->next;
+		n++;
+		h = h->next;
 	}
-
-	return count;
+	return (n);
 }
-
