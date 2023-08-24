c43b04cb1246a33b5fc285aeda869811171de402
adeoluseyi1 <adeoluseyi1@gmail.com>
2023-08-24 13:32:29 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,24 +11,20 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-	return NULL;
+	list_t *new;
+	unsigned int len = 0;
 
-	list_t *new_node = malloc(sizeof(list_t));
-		if (new_node == NULL)
-			return NULL;
+	while (str[len])
+		len++;
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-	return NULL;
-	}
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
-	new_node->len = strlen(str);
-	new_node->next = *head;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
-	*head = new_node;
-
-	return new_node;
+	return (*head);
 }
